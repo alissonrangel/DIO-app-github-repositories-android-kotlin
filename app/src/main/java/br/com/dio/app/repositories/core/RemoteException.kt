efa29286460c745c5fddd2fe407128d274083118
alissonrangel <alisson.rangel@hotmail.com>
2021-08-20 20:44:21 -0300

    final project with changes

---
+++ app/src/main/java/br/com/dio/app/repositories/core/RemoteException.kt
@@ -0,0 +1,7 @@
+package br.com.dio.app.repositories.core
+
+class RemoteException(
+    override val message: String
+) : Throwable() {
+
+}
---
+++ app/src/main/java/br/com/dio/app/repositories/data/model/Owner.kt
@@ -0,0 +1,11 @@
+package br.com.dio.app.repositories.data.model
+
+import com.google.gson.annotations.SerializedName
+
+data class Owner (
+    val login: String,
+    @SerializedName("avatar_url")
+    val avatarURL: String,
+    @SerializedName("html_url")
+    val htmlURL: String
+)
